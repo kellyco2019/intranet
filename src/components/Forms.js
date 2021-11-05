ec9b7bb80b851a4bd8725b2889163f2d673cc19b
LuisAaronHernandezRangel <lahr.4444@gmail.com>
2021-11-05 17:44:14 -0600

    formulario

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -13,7 +13,7 @@ function Forms() {
     <div class="shadow-md bg-white md:col-span-1 md:ml-6 h-80">
       <div class="">
         <img src="../intra.PNG" className="shadow-md" ></img>
-        <h1 class=" text-3xl font-medium leading-6 text-gray-900 my-6 mx-1">Formulario COVID</h1>
+        <h1 class=" text-3xl font-medium leading-6 text-gray-900 my-6 mx-1">Reporte Diario</h1>
         
         <p class="mx-1 text-sm text-gray-600">
           Use a permanent address where you can receive mail.
@@ -25,48 +25,115 @@ function Forms() {
         <div class="shadow overflow-hidden sm:rounded-md">
           <div class="px-4 py-5 bg-white sm:p-6">
             <div class="grid grid-cols-6 gap-6">
-              <div class="col-span-6 sm:col-span-3">
-                <label for="first_name" class="block text-sm font-medium text-gray-700">First name</label>
-                <input type="text" name="first_name" id="first_name" autocomplete="given-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
+
+
+
+              <div class="col-span-6 sm:col-span-6 lg:col-span-2">
+                <label for="cc" class="block text-sm font-medium text-gray-700">CC</label>
+                <input type="text" name="cc" id="cc" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
               </div>
 
-              <div class="col-span-6 sm:col-span-3">
-                <label for="last_name" class="block text-sm font-medium text-gray-700">Last name</label>
-                <input type="text" name="last_name" id="last_name" autocomplete="family-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
+              <div class="flex col-end-7 col-span-1 justify-center items-end">
+                <button type="button" className="rounded-full py-1 px-3 bg-gray-400 text-white">Buscar</button>
               </div>
 
-              <div class="col-span-6 sm:col-span-4">
-                <label for="email_address" class="block text-sm font-medium text-gray-700">Email address</label>
-                <input type="text" name="email_address" id="email_address" autocomplete="email" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
-              </div>
-
-              <div class="col-span-6 sm:col-span-3">
-                <label for="country" class="block text-sm font-medium text-gray-700">Country / Region</label>
-                <select id="country" name="country" autocomplete="country" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-                  <option>United States</option>
-                  <option>Canada</option>
-                  <option>Mexico</option>
+              <div class="col-start-1 col-end-6">
+                <label for="supervisor" class="block text-sm font-medium text-gray-700">Supervisor</label>
+                <select id="supervisor" name="supervisor" autocomplete="supervisor" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                  <option>Andrés Sánchez</option>
+                  <option>Carlos Calderon</option>
+                  <option>Felipe Torres</option>
+                  <option>Giovanni Castellanos</option>
+                  <option>Holman Duarte</option>
+                  <option>Jaime Lopez</option>
+                  <option>Jorge Becerra</option>
+                  <option>José Luis Alcántar</option>
+                  <option>Luis Carlos Quintero</option>
+                  <option>Miguel Garzon</option>
+                  <option>Nicolas Alfonso</option>
                 </select>
               </div>
+
+              <div class="col-span-6 sm:col-span-2">
+                <label for="last_name" class="block text-sm font-medium text-gray-700">Last name</label>
+                <input type="date" id="start" name="trip-start" value="2021-11-05" min="2021-11-05" max="2024-12-31" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"/>
+              </div>
+              <div class="col-span-6 sm:col-span-2">
+                <label for="estado" class="block text-sm font-medium text-gray-700">Estado</label>
+                <select id="estado" name="country" autocomplete="estado" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                  <option>Campo</option>
+                  <option>Compensatorio</option>
+                  <option>Curso HSE</option>
+                  <option>Digitalización de Planos</option>
+                  <option>Disponible en Hotel</option>
+                  <option>En Casa</option>
+                  <option>Informes</option>
+                  <option>Movilización-Conductor</option>
+                  <option>Movilización-Pasajero</option>
+                  <option>Oficina</option>
+                  <option>Permiso</option>
+                  <option>Trabajo en casa</option>
+                  <option>Vacaciones</option>
+                </select>
+              </div>
+
+              <div class="col-span-6 sm:col-span-2">
+                <label for="cliente" class="block text-sm font-medium text-gray-700">Cliente</label>
+                <select id="cliente" name="country" autocomplete="estado" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                  <option>CENIT LINEAS</option>
+                  <option>CENIT_PLANTAS</option>
+                  <option>PERENCO</option>
+                  <option>SGS</option>
+                  <option>STEPAN</option>
+                  <option>STORK</option>
+                  <option>VARIOS</option>
+                  <option>SIN ASIGNAR</option>
+                  <option>NO APLICA</option>
+                </select>
+              </div>
+
+              <div class="col-span-6 sm:col-span-3 lg:col-span-3">
+                <label for="lugar_de_inspeccion" class="block text-sm font-medium text-gray-700">Lugar de Inspección</label>
+                <input placeholder="Planta/Línea/Proyecto" type="text" name="lugar_de_inspeccion" id="lugar_de_inspeccion" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
+              </div>
+
+              <div class="col-span-6 sm:col-span-6">
+                <label for="dr_linea" class="block text-sm font-medium text-gray-700">DR-LÍNEA/TAG EQUIPO O SISTEMA TUBERÍA</label>
+                <input type="text" name="dr_linea" id="dr_linea" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
+              </div>
+
+              <div class="col-span-6 sm:col-span-3">
+                <label for="alcance" class="block text-sm font-medium text-gray-700">Alcance</label>
+                <input placeholder="Breve Descripción" type="text" name="alcance" id="alcance" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
 
               <div class="col-span-6">
                 <label for="street_address" class="block text-sm font-medium text-gray-700">Street address</label>
                 <input type="text" name="street_address" id="street_address" autocomplete="street-address" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
               </div>
 
-              <div class="col-span-6 sm:col-span-6 lg:col-span-2">
-                <label for="city" class="block text-sm font-medium text-gray-700">City</label>
-                <input type="text" name="city" id="city" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
+              <div class="col-span-6 sm:col-span-2">
+                <label for="tecnica" class="block text-sm font-medium text-gray-700">Técnica</label>
+                <input type="text" id="tecnica" name="tecnica" value="" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"/>
               </div>
 
-              <div class="col-span-6 sm:col-span-3 lg:col-span-2">
-                <label for="state" class="block text-sm font-medium text-gray-700">State / Province</label>
-                <input type="text" name="state" id="state" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
+              <div class="col-span-6 sm:col-span-2">
+                <label for="cantidad" class="block text-sm font-medium text-gray-700">cantidad</label>
+                <input type="text" id="cantidad" name="cantidad" value="" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"/>
               </div>
 
-              <div class="col-span-6 sm:col-span-3 lg:col-span-2">
-                <label for="postal_code" class="block text-sm font-medium text-gray-700">ZIP / Postal</label>
-                <input type="text" name="postal_code" id="postal_code" autocomplete="postal-code" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
+              <div class="col-span-6 sm:col-span-6">
+                <label for="disc" class="block text-sm font-medium text-gray-700">DESCRIPIÓN SEGÚN ITEM CONTRATO</label>
+                <select id="dsic" name="dsic" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                  <option>CENIT LINEAS</option>
+                  <option>CENIT_PLANTAS</option>
+                  <option>PERENCO</option>
+                  <option>SGS</option>
+                  <option>STEPAN</option>
+                  <option>STORK</option>
+                  <option>VARIOS</option>
+                  <option>SIN ASIGNAR</option>
+                  <option>NO APLICA</option>
+                </select>
               </div>
             </div>
           </div>
@@ -76,6 +143,7 @@ function Forms() {
             </button>
           </div>
         </div>
+       </div> 
       </form>
     </div>
   </div>
